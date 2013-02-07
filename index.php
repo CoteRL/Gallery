1b02a61e4ec0e8756101c6db63d7412536857fbf
CoteRL <coterl@gmail.com>
2013-02-06 21:35:25 -0500

    First

--- index.php
+++ index.php
@@ -1,151 +1,161 @@
 <?php
+###################
+$pageMax = 25;    #max images per page
+###################
+
 #Check if configuration file exists and require it if so.  If not, die
-if(!file_exists('includes/config.php')) {
+if(!file_exists('config.php')) {
 	die('Cannot locate configuration file');
 }
 else {
-	require('includes/config.php');
+	require('config.php');
 }
 #TinyButStrong template engine
-include_once('includes/tbs_class.php');
+include_once('tbs_class.php');
 $TBS = new clsTinyButStrong;
 
-##
-#Configuration values needed from config.php
-#$filepath
-#
-##
 
+$home = substr($_SERVER['PHP_SELF'],0,strlen($_SERVER['PHP_SELF'])-10); 					#.com/somedir/gallery/index.php TO /somedir/gallery
+$homePath = substr($_SERVER['SCRIPT_FILENAME'],0,strlen($_SERVER['SCRIPT_FILENAME'])-10); 	#/var/www/localhost/htdocs/somedir/gallery/index.php TO /var/www/localhost/htdocs/somedir/gallery
 
-#If it's a straight directory grab do the directory searching stuff
-if (!$_SERVER['QUERY_STRING']) {
+$request = urldecode(preg_replace('/\?.*/','',$_SERVER['REQUEST_URI']));					#/somedir/gallery/Hello%20World/?v=1 TO /somedir/gallery/Hello World/
 
-	##Grab and convert html characters from the uri requested minus any set variables
-	##/test/Hello%20World/?v=1 to '/test/Hello World/
-	$request = urldecode(preg_replace('/\?.*/','',$_SERVER['REQUEST_URI']));
-	
-	$working_dir = $FULL_PATH.$request;
-	$blank_thumb = $HOME.'/includes/folder.png';
-	
-	#Only do the stuff if the directory actually exists
-	if(file_exists($working_dir)) {
-		#Scan the directory for all files and directories but remove server configuration files and some other unwanted entries
-		#probably a better way to do this but i'm stupid
-		$results = array_diff(scandir($working_dir), $IGNORE);
+$dirRequest = substr($request,strlen($home),strlen($request));								#/somedir/gallery/Hello World/ TO /Hello World/
 
-		$directories = null;
-		$files = null;
-		
-		#Go through the full results and split the entries into two arrays; files and directories
-		foreach($results as $i => $result) {
-			if(is_dir($working_dir.$result)) {
+$picPath 	= $homePath.$_CFG['PIC_DIR'];													#/var/www/localhost/htdocs/somedir/gallery/pics
+$imageDir 	= $home.$_CFG['PIC_DIR'].$dirRequest;											#/somedir/gallery/pics
 
-				
-				#Find the latest added picture to use as a thumbnail for the folder
-				$last_mod = 0;
-				$last_mod_file = '';
-				$scan = $FULL_PATH.$request.$result.'/';
-				foreach (scandir($scan) as $entry) {
-					if (is_file($scan.$entry) && filectime($scan.$entry) > $last_mod) {
-						$last_mod = filectime($scan.$entry);
-						$last_mod_file = $entry;
+$workingDir = $picPath.$dirRequest;															#/var/www/localhost/htdocs/somedir/gallery/pics/Hello World/
+
+#Only do the stuff if the directory actually exists
+if(file_exists($workingDir)) {
+	$directories = array();
+	$images = array();
+	$videos = array();
+
+	$dirScan = new DirectoryIterator($workingDir);
+	$d = 0;
+	$i = 0;
+	$v = 0;
+	foreach ($dirScan as $fileInfo) {
+		if ($fileInfo->isDot()) continue;
+		if ($fileInfo->isDir()) {
+			$directories[$d]['name'] 		= $fileInfo->getFilename();
+			$directories[$d]['path'] 		= $request.$fileInfo->getFilename().'/';
+			
+			$lastMod = 0;
+			$lastModFile = '';
+			$subDir = $workingDir.$fileInfo->getFilename();
+			$subScan = new DirectoryIterator($subDir);
+			foreach ($subScan as $subInfo) {
+				if ($subInfo->isDot()) continue;
+				if ($subInfo->isDir()) continue;
+				if ($subInfo->isFile()) {
+					if (in_array(strtolower($subInfo->getExtension()), $_CFG['SUPPORTED_PICS']) && filectime($subInfo->getPathname()) > $lastMod) {
+						$lastMod = filectime($subInfo->getPathname());
+						#$lastModFile = $subInfo->getFilename();
+						$lastModFileThumb = $home.$_CFG['THUMB_DIR'].substr($subInfo->getPathname(),strlen($homePath.$_CFG['PIC_DIR']),strlen($subInfo->getPathname()));
 					}
 				}
-				
-				$directories[$i]['name'] 		= $result;
-				$directories[$i]['path'] 		= $request.$result;
-				
-				if(!$last_mod_file) {
-					$directories[$i]['thumb'] 		= 'No Thumb';
-					$directories[$i]['thumb_path']	= $HOME.'/includes/folder.png';
-				}
-				else {
-					$directories[$i]['thumb'] 		= $last_mod_file;
-					$directories[$i]['thumb_path']	= $request.$result.'/'.$last_mod_file;
-				}
-				
+			}
+			if(!$lastModFileThumb) {
+				$directories[$d]['thumb']	= $home.$_CFG['NO_THUMB'];
 			}
 			else {
-				$files[] = $result;
+				$directories[$d]['thumb']	= $lastModFileThumb;
+			}
+			$lastModFileThumb = NULL;
+			$d++;
+			continue;
+		}
+		if ($fileInfo->isFile()) {
+			if (in_array(strtolower($fileInfo->getExtension()), $_CFG['SUPPORTED_PICS'])) {
+				$images[$i]['name'] 	= $fileInfo->getFilename();
+				$images[$i]['path'] 	= $imageDir.$fileInfo->getFilename();
+				$images[$i]['thumb'] 	= $home.$_CFG['THUMB_DIR'].$dirRequest.$fileInfo->getFilename();
+				$i++;
+				continue;
+			}
+			if (in_array(strtolower($fileInfo->getExtension()), $_CFG['SUPPORTED_VIDS'])) {
+				$videos[$v]['name']		= $fileInfo->getFilename();
+				$v++;
+				continue;
 			}
 		}
-		
-		#GO THROUGH FILES AND ONLY GRAB SUPPORTED FILE TYPES
-		#if(!empty($files)){
-		#	foreach($files as $file) {
-		#		
-		#	}
-		#}
+	}
+	asort($directories);
+	asort($images);
+	#asort($videos);
+	
+	
+	if (!isset($_GET['page'])) {
+		$page=1;
+	}
+	else {
+		$page = $_GET['page'];
+	}
+	$pages = ceil(count($images) / 25);
+	$pagination = array();
+	$p = 1;
+	do {
+		$pagination[] = $p;
+		$p++;
+	} while ($p <= $pages);
 
-		#Create the nav breadcrumbs
-		$bread_crumb = null;
-		if($request != $HOME && $request != $HOME.'/') {
-			##Create array of all the bread_crumbs
-			#$bread_crumbs = array_diff(explode('/',$request), array(str_replace('/','',$HOME)));
-			##Remove any empty entries before the first / and after the last
-			#$bread_crumbs = array_filter($bread_crumbs, 'strlen');
-			
-			#foreach($bread_crumbs as $i => $crumb) {
-				#Find the current crumb in the full request uri to generate a link for it
-				##Fix the regex so it matches after the word instead of including the word, then remove .$crumb from the end of the next line... tard
-			#	preg_match("/$crumb.*/",$request,$match);
-			#	$bread_crumb[$i]['path'] = str_replace($match[0],'',$request).$crumb;
-			#	
-			#	$bread_crumb[$i]['name'] = $crumb;
-			#
-			#}
-			
-			$home2 = str_replace('/','',$HOME);
-			if( preg_match_all(';/([^/]*);', $request, $regs) ) {
-				for($i=0; $i<count($regs[0]); $i++) {
-					if($regs[1][$i] != $home2 && $regs[1][$i] != '') {
-						$name = $regs[1][$i];
-						$href = '';
-						
-						for($j=0; $j<$i+1; $j++) {
-							$href .= $regs[0][$j];
-						}
-						$bread_crumb[$i]['path'] = $href;
-						$bread_crumb[$i]['name'] = $name;
+	$offset = ($page - 1) * $pageMax;
+	$images = array_slice($images, $offset, $pageMax);
+	
+	#Create the nav breadcrumbs
+	$bread_crumb = null;
+	if ($request != $home && $request != $home.'/') {			
+		$home2 = str_replace('/','',$home);
+		if ( preg_match_all(';/([^/]*);', $request, $regs) ) {
+			for ($i=0; $i<count($regs[0]); $i++) {
+				if ($regs[1][$i] != $home2 && $regs[1][$i] != '') {
+					$name = $regs[1][$i];
+					$href = '';
+					
+					for ($j=0; $j<$i+1; $j++) {
+						$href .= $regs[0][$j];
 					}
+					$bread_crumb[$i]['path'] = $href.'/';
+					$bread_crumb[$i]['name'] = $name;
 				}
 			}
 		}
-		
-		
-		#Load specific template
-		$TBS->LoadTemplate('templates/main.htm');
-		
-		#Specify some page specific variables
-		$page_title = '';
+	}
 	
-		if (is_null($bread_crumb)) $bread_crumb = array();
-		$TBS->MergeBlock('bread', $bread_crumb);
-		
-		if (is_null($directories)) $directories = array();
-		$TBS->MergeBlock('dirs', $directories);
-		
-		if (is_null($files)) $files = array();
-		$TBS->MergeBlock('files', $files);
-		
-		$TBS->Show();
-		
-		
-	}
-	else {
-		#Load specific template
-		$TBS->LoadTemplate('templates/error.htm');
-		
-		#Specify some page specific variables
-		$page_title = 'Oops!';
-		$error = 'Sorry, that directory does not exist!';
-		
-		$TBS->Show();
-	}
-}
-#If there IS a query then parse it
-else {
-#tags, etc to go here
-}
+	#Load specific template
+	$TBS->LoadTemplate('templates/main.htm');
+	
+	#Specify some page specific variables
+	$page_title = '';
 
+	if (is_null($bread_crumb)) $bread_crumb = array();
+	$TBS->MergeBlock('bread', $bread_crumb);
+	
+	if (is_null($directories)) $directories = array();
+	$TBS->MergeBlock('dirs', $directories);
+	
+	if (is_null($images)) $images = array();
+	$TBS->MergeBlock('images', $images);
+	
+	if (is_null($videos)) $videos = array();
+	$TBS->MergeBlock('videos', $videos);
+	
+	if (is_null($pagination)) $pagination = array();
+	$TBS->MergeBlock('pagination', $pagination);
+	
+	$TBS->Show();
+}
+else {
+	#Load specific template
+	$TBS->LoadTemplate('templates/error.htm');
+	
+	#Specify some page specific variables
+	$page_title = 'Oops!';
+	$error = 'Sorry, that directory does not exist!';
+	
+	$TBS->Show();
+}
 ?>
